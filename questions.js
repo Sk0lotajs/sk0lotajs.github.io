86bdf53c1231ecfbf9c2ed31b2eddc5af7816052
Sk0lotajs <bogdanovs.grigorijs@gmail.com>
2025-11-23 13:53:54 +0200

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,46 @@
+const questions = {
+    1: [
+    { type: "choice",
+    question: "Kāds veids Tev vislabāk palīdz saprast jaunu tēmu?",
+    options: ["Lasīt un pierakstīt", "Klausīties skaidrojumu", "Skatīties prezentāciju", "Praktiski darīt/risināt uzdevumus", "Apspriest ar citiem"] },
+    { type: "choice", 
+    question: "Kad Tev visvieglāk mācīties?",
+    options: ["No rīta", "Pēcpusdienā", "Vakarā"] },
+    ],
+    2: [
+    { type: "choice",
+    question: "Kas tev palīdz koncentrēties mācību laikā?",
+    options: ["Klusums", "Mūzika", "Darbs kopā ar draugiem", "Īsi pārtraukumi", "Skaidrs plāns"] },
+    { type: "text", question: "Kuri priekšmeti tev ir visinteresantākie? (Var ierakstīt vairākus)" },
+    ],
+    3: [
+    { type: "text", question: "Ar ko tu labprāt nodarbojies ārpus skolas?" },
+    { type: "text", question: "Vai tev ir mērķis vai sapnis, kura dēļ ir svarīgi labi mācīties?" },
+    ],
+    4: [
+    { type: "choice",
+    question: "Ko tu parasti dari, ja nesaproti tēmu no pirmās reizes?",
+    options: ["Pārlasu/pārskatu vēlreiz", "Jautāju skolotājam", "Meklēju informāciju internetā", "Apspriežu ar klasesbiedriem", "Vienkārši eju tālāk"] },
+    { type: "choice",
+    question: "Kā tu jūties par grupu darbu klasē?",
+    options: ["Man patīk strādāt kopā ar citiem", "Es labāk strādāju viens/viena", "Tas atkarīgs no tēmas un grupas"] },
+    ],
+    5: [
+    { type: "choice",
+    question: "Kas tev palīdz neatlikt mājasdarbus?",
+    options: ["Termiņa tuvošanās", "Interese par tēmu", "Vecāku atbalsts", "Laika plānošana", "Mūzika/motivācija"] },
+    { type: "choice",
+    question: "Kā tev ērtāk saņemt skolotāja komentārus?",
+    options: ["Mutiski", "Rakstiski (piemēram, piezīmes vai e-vidē)", "Klases diskusijā", "Individuālā sarunā"] },
+    ],
+    22: [
+    { type: "choice",
+    question: "Vai matemātika ir demokrātiskā valsts?",
+    options: ["Jā", "Nē"]
+    },
+    ]
+};
+
+const activities = {
+    8: "Šaha spēle"
+};
